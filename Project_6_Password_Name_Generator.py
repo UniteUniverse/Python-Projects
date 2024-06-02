0e975ec7385b43f11435c55f519f39362ec9bb69
Pratyush Kumar Jha <122214040+UniteUniverse@users.noreply.github.com>
2024-06-02 11:26:13 +0530

    Add files via upload
    Here I have created a password generator. In this you will get two options of simple and hard passwords.

---
+++ Project_6_Password_Name_Generator.py
@@ -0,0 +1,30 @@
+import random
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+print("Welcome to the PyPassword Generator!")
+nr_letters= int(input("How many letters would you like in your password?\n")) 
+nr_symbols = int(input(f"How many symbols would you like?\n"))
+nr_numbers = int(input(f"How many numbers would you like?\n"))
+letter=[]
+for n in range(0,nr_letters):
+    letter.append(random.choice(letters))
+
+
+num=[]
+for n in range(0,nr_numbers):
+    num.append(random.choice(numbers))
+
+
+sym=[]
+for n in range(0,nr_symbols):
+    letter.append(random.choice(symbols))
+
+
+simplepassword=letter+sym+num
+print("Simple Password")
+print("".join(simplepassword))
+random.shuffle(simplepassword)
+print("Hard Password")
+print("".join(simplepassword))
